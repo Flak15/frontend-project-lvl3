f612e593edb7abee05930a7f246953d7899b7598
flak15 <flak160@gmail.com>
2019-07-04 17:29:50 +0300

    fix

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'RSSReader',
+      template: 'template.html',
+    }),
+  ],
+};
