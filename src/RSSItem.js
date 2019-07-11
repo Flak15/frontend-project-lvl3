8e1919823aef40341d5668c9149f6db944e2ff3a
flak15 <flak160@gmail.com>
2019-07-11 15:57:36 +0300

    fix sort

--- src/RSSItem.js
+++ src/RSSItem.js
@@ -1,14 +1,13 @@
-const parseXMLTextElement = el => el.childNodes[0].nodeValue;
-// Array.from(el.childNodes).map(child => child.nodeValue).join();
+const parseXMLTextElement = el => Array.from(el.childNodes).map(child => child.nodeValue).join('');
 
 export default class RSSItem {
-  constructor(xmlItem) {
+  constructor(xmlItem, source) {
+    this.source = source.innerHTML;
     this.title = parseXMLTextElement(xmlItem.querySelector('title'));
     this.description = parseXMLTextElement(xmlItem.querySelector('description'));
     this.link = xmlItem.querySelector('link').innerHTML;
     this.pubDate = new Date(xmlItem.querySelector('pubDate').innerHTML);
   }
-
 
   render() {
     const jumbotron = document.createElement('div');
@@ -17,8 +16,10 @@ export default class RSSItem {
         <h1 class="display-4">${this.title}</h1>
         <p class="lead">${this.description}</p>
         <hr class="my-4">
-        <p>${(this.pubDate.toDateString())}</p>
+        <p>${this.pubDate.toDateString()}</p>
+        <p>Источник: ${this.source}</p>
         <a class="btn btn-primary btn-lg" href="${this.link}" role="button">Learn more</a>
+        <a class="btn btn-info btn-lg" href="${this.link}" role="button" id="description">Show more</a>
       </div>`;
     return jumbotron;
   }
--- src/init.js
+++ src/init.js
@@ -10,51 +10,52 @@ export default () => {
     items: [],
   };
 
-  // const button = document.querySelector('#butt');
   const addButton = document.querySelector('#add');
   const container = document.querySelector('#mount');
   const input = document.querySelector('#basic-url');
 
   WatchJS.watch(state, 'items', () => {
-    const items = state.items;
-    // items.sort((a, b) => {
-    //   a.pubDate < b.pubDate;
-    // });
+    const { items } = state;
     items.map(item => container.append(item.render()));
-    //
   });
 
   const request = (url) => {
-    axios.get(url).then(res => {
+    axios.get(url).then((res) => {
       const parser = new DOMParser();
       const document = parser.parseFromString(res.data, 'application/xml');
       const items = document.querySelectorAll('item');
-      const newItems = Array.from(items).map(item => new RSSItem(item));
-      state.items = [...state.items, ...newItems].sort((a, b) => a.pubDate < b.pubDate);
+      const source = document.querySelector('channel title');
+      const newItems = Array.from(items).map(item => new RSSItem(item, source));
+      state.items = [...state.items, ...newItems].sort((a, b) => a.pubDate - b.pubDate);
     });
-  }
+  };
 
-  input.addEventListener('change', () =>  {
+  input.addEventListener('change', () => {
     state.input = input.value;
     state.valid = validator.isURL(state.input) && !state.sources.includes(state.input);
     addButton.disabled = !state.valid;
     if (state.valid) {
       input.classList.remove('is-invalid');
       input.classList.add('is-valid');
+      addButton.classList.remove('btn-secondary', 'btn-danger');
+      addButton.classList.add('btn-success');
     } else {
       addButton.disabled = true;
       input.classList.remove('is-valid');
       input.classList.add('is-invalid');
+      addButton.classList.remove('btn-secondary', 'btn-success');
+      addButton.classList.add('btn-danger');
     }
   });
 
   addButton.addEventListener('click', () => {
-    if (state.valid) {
-      request(`https://cors-anywhere.herokuapp.com/${state.input}`);
-      state.sources = [...state.sources, state.input];
-      input.value = '';
-    } else {
-      alert('wrong URL');
-    }
+    request(`https://cors-anywhere.herokuapp.com/${state.input}`);
+    state.sources = [...state.sources, state.input];
+    input.value = '';
+    input.classList.remove('is-valid');
+    addButton.classList.remove('btn-success');
+    addButton.classList.add('btn-secondary');
   });
-}
+
+  document.querySelectorAll('#description').forEach(descrButton => descrButton.addEventListener('mouseover', () => alert('mouseover')));
+};
