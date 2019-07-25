46aae89551662d6685bf4f7c6147ada04ee57b15
flak15 <flak160@gmail.com>
2019-07-25 12:45:49 +0300

    refact

--- src/init.js
+++ src/init.js
@@ -10,13 +10,54 @@ export default () => {
     items: [],
     newItems: [],
     lastUpdate: 0,
-    inputStatus: 'empty'
+    inputStatus: 'empty',
   };
 
-  const addButton = document.querySelector('#add');
+  const buttonAddSource = document.querySelector('#add');
   const container = document.querySelector('#mount');
   const input = document.querySelector('#basic-url');
   const info = document.querySelector('#info');
+
+  const getRSSFeed = url => axios.get(`https://cors-anywhere.herokuapp.com/${url}`).then((res) => {
+    const parser = new DOMParser();
+    return parser.parseFromString(res.data, 'application/xml');
+  });
+
+  const getItems = (xmlDoc) => {
+    const items = xmlDoc.querySelectorAll('item');
+    const source = xmlDoc.querySelector('channel title');
+    return Array.from(items).map(item => new RSSItem(item, source));
+  };
+
+  const addNewSource = (url) => {
+    state.sources = [...state.sources, url];
+    getRSSFeed(url).then((document) => {
+      const newItems = getItems(document);
+      state.newItems = newItems;
+      info.innerHTML = '';
+    });
+  };
+
+  const updateItems = () => {
+    if (state.sources.length === 0) return;
+    const feeds = state.sources.map(source => getRSSFeed(source));
+    Promise.all(feeds).then(docs => docs.map((document) => {
+      const newItems = getItems(document).filter(item => item.pubDate > state.lastUpdate);
+      state.newItems = newItems;
+      return null;
+    }));
+  };
+
+  input.addEventListener('change', () => {
+    state.inputValue = input.value;
+    state.inputStatus = validator.isURL(state.inputValue) && !state.sources.includes(state.inputValue) ? 'valid' : 'invalid';
+  });
+
+  buttonAddSource.addEventListener('click', () => {
+    info.innerHTML = 'Загрузка...';
+    addNewSource(state.inputValue);
+    state.inputStatus = 'empty';
+  });
 
   WatchJS.watch(state, 'newItems', () => {
     const { items, newItems } = state;
@@ -26,77 +67,32 @@ export default () => {
     state.lastUpdate = state.items[0].pubDate;
   });
 
-  const getRSSFeed = url => axios.get(`https://cors-anywhere.herokuapp.com/${url}`).then((res) => {
-    const parser = new DOMParser();
-    return parser.parseFromString(res.data, 'application/xml');
-  });
-
-
-  const addNewSource = (url) => {
-    state.sources = [...state.sources, url];
-    getRSSFeed(url).then((document) => {
-      const items = document.querySelectorAll('item');
-      const source = document.querySelector('channel title');
-      const newItems = Array.from(items).map(item => new RSSItem(item, source));
-      state.newItems = newItems;
-      info.innerHTML = '';
-    });
-  };
-
-  const update = () => {
-    if (state.sources.length === 0) return;
-    const feeds = state.sources.map(source => getRSSFeed(source));
-    Promise.all(feeds).then(docs => docs.map((document) => {
-      const items = document.querySelectorAll('item');
-      const source = document.querySelector('channel title');
-      const newItems = Array.from(items)
-        .map(item => new RSSItem(item, source))
-        .filter(item => item.pubDate > state.lastUpdate);
-      state.newItems = newItems;
-      return null;
-    }));
-  };
-
-  setInterval(update, 5000);
-
   WatchJS.watch(state, 'inputStatus', () => {
-
     const inputStatusHandlers = {
       valid() {
-        addButton.disabled = false;
+        buttonAddSource.disabled = false;
         input.classList.remove('is-invalid');
         input.classList.add('is-valid');
-        addButton.classList.remove('btn-secondary', 'btn-danger');
-        addButton.classList.add('btn-success');
+        buttonAddSource.classList.remove('btn-secondary', 'btn-danger');
+        buttonAddSource.classList.add('btn-success');
       },
       invalid() {
-        addButton.disabled = true;
+        buttonAddSource.disabled = true;
         input.classList.remove('is-valid');
         input.classList.add('is-invalid');
-        addButton.classList.remove('btn-secondary', 'btn-success');
-        addButton.classList.add('btn-danger');
+        buttonAddSource.classList.remove('btn-secondary', 'btn-success');
+        buttonAddSource.classList.add('btn-danger');
       },
       empty() {
-        addButton.disabled = true;
+        buttonAddSource.disabled = true;
         input.value = '';
         input.classList.remove('is-valid');
-        addButton.classList.remove('btn-success');
-        addButton.classList.add('btn-secondary');
-      }
+        buttonAddSource.classList.remove('btn-success');
+        buttonAddSource.classList.add('btn-secondary');
+      },
     };
     inputStatusHandlers[state.inputStatus]();
   });
 
-  input.addEventListener('change', () => {
-    state.input = input.value;
-    state.inputStatus = validator.isURL(state.input) && !state.sources.includes(state.input) ? 'valid' : 'invalid';
-    // alert(state.inputStatus);
-  });
-
-  addButton.addEventListener('click', () => {
-    info.innerHTML = 'Загрузка...';
-    addNewSource(state.input);
-    state.inputStatus = 'empty';
-
-  });
+  setInterval(updateItems, 5000);
 };
