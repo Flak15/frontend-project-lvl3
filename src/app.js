b0c3bd5690d50a70dd42438c73b04ca10c2b7f7c
Flak15 <flak160@gmail.com>
2020-08-11 12:37:57 +0300

    sync

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import onChange from 'on-change';
 import * as yup from 'yup';
 import axios from 'axios';
 import i18next from 'i18next';
+import _ from 'lodash';
 
 export default () => {
 	const state = {
@@ -18,51 +19,62 @@ export default () => {
 		feedBackContainer: document.querySelector('#invalid-feedback'),
 		form: document.querySelector('#rssInputAddressForm'),
 	};
+	const getRSSFeed = url => axios.get(`https://cors-anywhere.herokuapp.com/${url}`).then(res => res.data);
 	const watcherSelector = {
 		inputState(value) {
-			elements.feedBackContainer.innerHTML = value;
-			if (value === 'invalid') {
-				elements.feedBackContainer.innerHTML = state.errors.join(', ');
+			if (value === 'valid') {
+				elements.input.classList.remove('is-invalid');
+				elements.input.classList.add('is-valid');
+				elements.button.disabled = false;
+			} else if (value === 'invalid') {
+				elements.input.classList.add('is-invalid');
+				elements.input.classList.remove('is-valid');
+				elements.button.disabled = true;
+			} else if (value === 'idle') {
+				elements.input.classList.remove('is-valid');
+				elements.input.value = '';
+			} else {
+
 			}
 		},
 		inputValue() {},
-		urls() {
+		urls(value, prevValue) {
+			const newUrlId = _.xor(_.keys(value), _.keys(prevValue));
 
 		},
-		inputState() {
-
+		errors(value) {
+			elements.feedBackContainer.innerHTML = value.join(', ');
 		},
-		errors() {
-
-		},
-		schema() {
-
-		}
+		schema() {}
 	};
+
+
 
 	const getSchema = () => yup.object().shape({ url: yup.string().url().notOneOf(state.urls).required() });
 	const watchedState = onChange(state, (path, value, prevValue) => {
-		console.log(path);
 		watcherSelector[path](value, prevValue);
 	});
 	elements.form.addEventListener('submit', (e) => {
 		e.preventDefault();
+		
 		if (state.inputState === 'valid') {
-			watchedState.urls.push(watchedState.inputValue);
+			watchedState.urls.push({ [_.uniqueId()]: state.inputValue });
 			watchedState.schema = getSchema();
 			watchedState.inputState = 'idle';
+			
 		}
 	});
 	elements.input.addEventListener('input', (e) => {
 		e.preventDefault();
 		watchedState.inputValue = e.target.value;
 		state.schema.validate({ url: e.target.value })
-			.then(() => watchedState.inputState = 'valid')
+			.then(() => {
+				watchedState.errors = [];
+				watchedState.inputState = 'valid';
+			})
 			.catch(err => {
 				watchedState.errors = err.errors;
 				watchedState.inputState = 'invalid';
-				
-				
 			});
 
 	});
