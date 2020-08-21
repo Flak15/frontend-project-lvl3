d507e773056b63c9bdc294f5bf76395c295257ed
Flak15 <flak160@gmail.com>
2020-08-21 21:38:23 +0300

    src list, err proc, etc

--- src/app.js
+++ src/app.js
@@ -3,12 +3,21 @@ import * as yup from 'yup';
 import axios from 'axios';
 import i18next from 'i18next';
 import _ from 'lodash';
+import ru from './locales/ru.js';
+/** 
+ * 1. Add internationalization - DONE
+ * 2. Add sources list with deletion -DONE
+ * 3. Fix layout
+ * 4. Error proccessing
+ * 5. Add waiting circe while 'loading'
+ * 6. Pictures in posts
+ * */ 
 
 export default () => {
 	const state = {
 		inputValue: '',
 		inputState: 'idle',
-		urls: [], // { id: (num), url: (URL), updateDate: (DATE) }
+		sources: [], // { id: (num), url: (URL), updateDate: (DATE), name: (string) }
 		posts: [], // { sourseId: (num), id: (num), title: (string), description: (string), link: (URL), pubDate: (DATE) }
 		schema: yup.object().shape({ url: yup.string().url().required() }),
 		errors: []
@@ -19,12 +28,29 @@ export default () => {
 		mountContainer: document.querySelector('#mount'),
 		feedBackContainer: document.querySelector('#invalid-feedback'),
 		form: document.querySelector('#rssInputAddressForm'),
+		sourceList: document.querySelector('#source-list'),
 	};
-
-	const getRssFeed = url => axios.get(`https://cors-anywhere.herokuapp.com/${url}`, { origin: '' }).then(res => res.data);
-	
+	i18next.init({
+		lng: 'ru',
+		debug: true,
+		resources: { ru },
+		interpolation: {
+			format(value, format, lng) {
+				if (value instanceof Date) return new Intl.DateTimeFormat(format, {
+					hour: 'numeric',
+					minute: 'numeric',
+					second: 'numeric',
+					weekday: "long",
+					day:'numeric',
+					month:'long',
+					year: 'numeric'
+				}).format(value);
+				return value;
+			}
+		}
+	});
+	const getRssFeed = url => axios.get(`https://cors-anywhere.herokuapp.com/${url}`).then(res => res.data);
 	const parseRss = rawRssString => (new DOMParser).parseFromString(rawRssString, 'application/xml');
-	const parseXMLTextElement = element => Array.from(element.childNodes).map(child => child.nodeValue).join('');
 	const getRssItems = (xmlDoc, sourceId) => {
 		const items = xmlDoc.querySelectorAll('item');
 		return Array.from(items).map(item => parseItem(item, sourceId));
@@ -32,8 +58,8 @@ export default () => {
 	const  parseItem = (xmlItem, sourceId) => {
 		return {
 			sourceId,
-			title: parseXMLTextElement(xmlItem.querySelector('title')),
-			description: parseXMLTextElement(xmlItem.querySelector('description')),
+			title: xmlItem.querySelector('title').textContent,
+			description:xmlItem.querySelector('description').textContent,
 			link: xmlItem.querySelector('link').innerHTML,
 			pubDate: new Date(xmlItem.querySelector('pubDate').innerHTML),
 			id: _.uniqueId(),
@@ -52,21 +78,39 @@ export default () => {
 			} else if (value === 'idle') {
 				elements.input.classList.remove('is-valid');
 				elements.input.value = '';
+				elements.input.disabled = false;
+				elements.button.disabled = false;
+			} else if (value === 'loading') {
+				elements.input.classList.remove('is-valid');
+				elements.input.disabled = true;
+				elements.button.disabled = true;
 			} else {
 				throw new Error('Unknown state: ', value);
 			}
 		},
 		inputValue() {},
-		urls(value, prevValue) {
-			const [newSource] = _.xor(value, prevValue);
-			getRssFeed(newSource.url).then((rawRss) => {
-				const xmlDoc = parseRss(rawRss);
-				newSource.name = xmlDoc.querySelector('channel title').innerHTML;
-				newSource.lastUpdate = new Date();
-				const newPosts = getRssItems(xmlDoc, newSource.id);
-				watchedState.posts = [...state.posts, ...newPosts];
+		sources(value, prevValue) {
+			elements.sourceList.innerHTML = '';
+			state.sources.forEach((source) => {
+				const div = document.createElement('div');
+				div.classList.add('mb-3');
+				div.innerHTML = `
+					<button type="button" class="close" aria-label="Close" id="${source.id}">
+						<span aria-hidden="true">&times;</span>
+					</button>
+					<div>${source.name}</div>`;
+				elements.sourceList.appendChild(div);
+			});
+			document.querySelectorAll('.close').forEach((closeBtn) => {
+				closeBtn.addEventListener('click', (e) => {
+					const btn = e.target.closest('.close');
+					watchedState.sources = state.sources.filter(source => source.id !== btn.id);
+					watchedState.posts = state.posts.filter(post => post.sourceId !== btn.id);
+					watchedState.schema = getSchema(state.sources);
+				});
 			});
 		},
+		
 		errors(value) {
 			elements.feedBackContainer.innerHTML = value.join(', ');
 		},
@@ -85,30 +129,57 @@ export default () => {
 				<div class="card-body">
 					<h5 class="card-title"><a href="${post.link}">${post.title}</a></h5>
 					<hr class="my-4">
-					<p class="card-text">${post.pubDate}</p>
-					<p class="card-text">Источник: ${state.urls.find(source => source.id === post.sourceId).name}</p>
+					<p class="card-text">${post.description}</p>
+					${post.img ? 'img' : ''}
+					<p class="card-text">${i18next.t('date', { date: post.pubDate, language: i18next.language })}</p>
+					<p class="card-text">${i18next.t('source')}: ${state.sources.find(source => source.id === post.sourceId).name}</p>
 				</div>`;
 			elements.mountContainer.appendChild(div);
 		});
 	};
-	const updatePosts = () => {
-		state.urls.forEach(source => {
-			getRssFeed(source.url).then((rawRss) => {
-				const xmlDoc = parseRss(rawRss);
-				
-				
-				// const newPosts = getRssItems(xmlDoc, source.id);
-
-				// watchedState.posts = [...state.posts, ...newPosts];
-			});
-		})
+	const getNewPosts = (xmlDoc, source) => {
+		const items = xmlDoc.querySelectorAll('item');
+		const newItems = Array.from(items).map(xmlItem => {
+			const pubDate = new Date(xmlItem.querySelector('pubDate').innerHTML);
+			if (pubDate < source.updateDate)
+				return ;
+			return {
+				sourceId: source.id,
+				title: xmlItem.querySelector('title').textContent,
+				description: xmlItem.querySelector('description').textContent,
+				link: xmlItem.querySelector('link').innerHTML,
+				pubDate,
+				id: _.uniqueId(),
+			}
+		});
+		return _.compact(newItems);
 	};
-	//<p class="card-text">${format(post.pubDate, 'dd MMMM yyyy', { locale: ru })}</p>
-//${this.getModal(this)}
+	const updatePosts = () => {
+		console.log('Update', new Date());
+		if (state.sources.length === 0) {
+			setTimeout(updatePosts, 5000);
+			return;
+		}
+			
+		const newPostPromises = state.sources.map(source => {
+			return getRssFeed(source.url).then(rssString => {
+				const xmlDoc = parseRss(rssString);
+				const newPosts = getNewPosts(xmlDoc, source);
+				source.updateDate = new Date();
+				return newPosts;
+			});
+		});
+		Promise.all(newPostPromises).then((postsArrays) => {
+			postsArrays.forEach(newPosts => {
+				watchedState.posts = _.union(state.posts, newPosts);
+			});
+		}).catch(e => console.log('error: ', e))
+			.then(() => setTimeout(updatePosts, 5000));
+	};
 
+	const getSchema = (sources) => yup.object()
+		.shape({ url: yup.string().url().notOneOf(sources.map(source => source.url)).required() });
 
-	const getSchema = () => yup.object()
-		.shape({ url: yup.string().url().notOneOf(state.urls.map(source => source.url)).required() });
 	const watchedState = onChange(state, (path, value, prevValue) => {
 		watcherSelector[path](value, prevValue);
 	});
@@ -116,10 +187,22 @@ export default () => {
 		e.preventDefault();
 		
 		if (state.inputState === 'valid') {
-			watchedState.urls.push({ id: _.uniqueId(), url: state.inputValue });
-			watchedState.schema = getSchema();
-			watchedState.inputState = 'idle';
-			
+			const newUrl = state.inputValue;
+			const newSource = { id: _.uniqueId(), url: newUrl };
+			watchedState.inputState = 'loading';
+			getRssFeed(newUrl).then((rawRss) => {
+				const xmlDoc = parseRss(rawRss);
+				newSource.name = xmlDoc.querySelector('channel title').textContent;
+				newSource.updateDate = new Date();
+				watchedState.sources = [...state.sources, newSource];
+				watchedState.schema = getSchema(state.sources);
+				const newPosts = getRssItems(xmlDoc, newSource.id);
+				watchedState.posts = [...state.posts, ...newPosts];
+				watchedState.inputState = 'idle';
+			}).catch(e => {
+				watchedState.inputState = 'idle';
+				console.log('error: ', e);
+			});
 		}
 	});
 	elements.input.addEventListener('input', (e) => {
@@ -136,4 +219,5 @@ export default () => {
 			});
 
 	});
+	setTimeout(updatePosts, 5000);
 };
---
+++ src/locales/ru.js
@@ -0,0 +1,6 @@
+export default {
+	translation: {
+		source: 'Источник',
+		date: 'Опубликовано: {{date, lng}}'
+	},
+}
